0c1b4e5bd2951edbe8b90a3bba22e7b91e77cb3d
Tommy Crews <twcrews96@gmail.com>
2026-03-15 22:00:04 -0500

    Reduce scroll reveal threshold.

--- src/hooks/useScrollReveal.ts
+++ src/hooks/useScrollReveal.ts
@@ -18,7 +18,7 @@ export function useScrollReveal<T extends HTMLElement>(): RefObject<T | null> {
 					}
 				});
 			},
-			{ threshold: 0.5, rootMargin: '0px 0px -40px 0px' }
+			{ threshold: 0.2, rootMargin: '0px 0px -40px 0px' }
 		);
 
 		elements.forEach((el) => observer.observe(el));
